fec94f4b17ea7c3c8889361c7016c176466408f5
atiq012 <shakilratiq@gmail.com>
2025-04-24 18:25:06 +0600

    no flight search result

--- resources/js/components/search/noSearchResult.vue
+++ resources/js/components/search/noSearchResult.vue
@@ -27,21 +27,6 @@ import { useAuthStore } from '../../stores/authStore';
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <!-- <div class="row">
-                        <div class="col-md-12 text-center mb-0 pb-0">
-                            <img style="width: 75%; height: 75%;"
-                                src="../../../../public/theme/appimages/Serach_Reult_Not_Found.jpg" alt="">
-                        </div>
-                        <div class="col-md-12 text-center mt-0 pb-0">
-                            <h4 class="text-danger">We didn't find any flight based on your search !!!</h4>
-                            <p class="text-muted">Please try again changing the search details.</p>
-                        </div>
-                        <div class="col-md-12 text-center mt-0 pb-0">
-                            <router-link :to="{ name: 'searchResult' }" class="btn bluesky-btn-primary">Search
-                                Again</router-link>
-                        </div>
-                    </div> -->
-
                     <div class="d-flex flex-column bd-highlight mb-3">
                         <div class="text-center mb-0 pb-0">
                             <img style="width: 49%; height: 49%;"
